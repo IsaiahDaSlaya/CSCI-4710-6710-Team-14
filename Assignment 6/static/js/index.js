0f5254b66d12307e4bee67784653d35a3c676e1f
BearXinyu <47724732+BearXinyu@users.noreply.github.com>
2019-04-02 22:34:19 -0400

    Update index.js

--- Assignment 6/static/js/index.js
+++ Assignment 6/static/js/index.js
@@ -1,9 +1,9 @@
+
 $(document).ready(function(){
-	$('#random_button').click(function(){
-		$.get( "/api/random_number", function( data ) {
-		  // you need to parse the required data first
-		  data_json = JSON.parse(data);
-		  $( "#random_num_p" ).text( data_json['number'] );
-		});
+
+	$('#continue').click(function(){
+		window.location.href = "/verify";
+		
 	});
+
 });
