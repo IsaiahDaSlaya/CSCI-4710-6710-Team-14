a612d6d2819fa62f82f58d1c943cf4b752c895f3
Isaiah <binfordi14@students.ecu.edu>
2019-04-03 23:42:17 -0400

    fixed file upload error

--- Assignment 6/main.py
+++ Assignment 6/main.py
@@ -56,7 +56,30 @@ def upload_file():
 	elif request.method == 'GET':
 		return render_template('index.html')
 
-
+@app.route('/api/post_csv', methods=['POST'])
+def post_csv():
+	# request.file <class 'werkzeug.datastructures.FileStorage'>
+	# request.url is http://127.0.0.1:5000/api/post_csv
+	# check if the post request has the file part
+	if 'file' not in request.files:
+		log = 'no file field in request.'
+		return render_template('fail.html', log = log)
+	# print(request.files['file'])
+	file = request.files['file']
+	# if user does not select file, browser also
+	# submit an empty part without filename
+	if file.filename == '':
+		log = 'Empty filename.'
+		return render_template('fail.html', log = log)
+	if file and util.allowed_file(file.filename):
+		# get filename in a safe way
+		filename = secure_filename(file.filename)
+		# check if the data folder exists, if not create one
+		if os.path.exists(app.config['UPLOAD_FOLDER']) == False:
+			os.makedirs(app.config['UPLOAD_FOLDER'])
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		return render_template('success.html',filename=filename)
+		
 @app.route('/read_data')
 def read_data():
     return app.config['json_obj']
