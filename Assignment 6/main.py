f180e6392fa9bd576a7d6ef8f28b0f7a940d4cd3
Isaiah <binfordi14@students.ecu.edu>
2019-04-03 22:37:31 -0400

    added file upload funtionality

--- Assignment 6/main.py
+++ Assignment 6/main.py
@@ -1,21 +1,50 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
+from werkzeug.utils import secure_filename
 import json
 import util
 
-
 app = Flask(__name__)
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 UPLOAD_FOLDER = dir_path + '/data/'
+
+# Upload a CSV File
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 app.config['DATA_FILE'] = UPLOAD_FOLDER + 'NRDC_data.csv'
 app.config['COL_NAME'] = 'Temperature'
 app.config['json_obj'] = util.read_data(app.config['DATA_FILE'])
 
-@app.route('/')
-def index():
-    # this is your index page
-    log = 'Index.'
-    return render_template('index.html', log_index = log)
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+	if request.method == 'POST':
+		# request.file <class 'werkzeug.datastructures.FileStorage'>
+		# request.url is http://127.0.0.1:5000/
+		# check if the post request has the file part
+		if 'file' not in request.files:
+			log = 'no file field in request.'
+			return render_template('fail.html', log = log)
+		# print(request.files['file'])
+		file = request.files['file']
+		# if user does not select file, browser also
+		# submit an empty part without filename
+		if file.filename == '':
+			# This part should use flash to output information
+			log = 'Empty filename.'
+			return render_template('fail.html', log = log)
+		if file and util.allowed_file(file.filename):
+			# get filename in a safe way
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			column_names, data_part = util.preview_csv(app.config['UPLOAD_FOLDER']+filename)
+			return render_template('success.html',column_names=column_names, data_part=data_part)
+	elif request.method == 'GET':
+		return render_template('index.html')
+
+
+
 @app.route('/read_data')
 def read_data():
     return app.config['json_obj']
--- Assignment 6/util.py
+++ Assignment 6/util.py
@@ -17,3 +17,30 @@ def threshold_process_method(filename, col_name, lower_threshold, upper_threshol
 def read_data(filename):
   df = pd.read_csv(filename)
   return df.to_json()
+
+# if a file extension is not listed, the system will not upload the file
+ALLOWED_EXTENSIONS = set(['csv'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def preview_csv(filename):
+	'''
+	this function will return a JSON file
+	containing the first 5 lines of a CSV file
+	'''
+	# read csv file
+	df = pd.read_csv(filename)
+	# this will return the total length of your CSV file
+	total_len = len(df)
+	csv_values = []
+	# if length is less than 5
+	if total_len < 5:
+		csv_values = df.values.tolist()
+	else:
+		csv_values = df.head(n=5).values.tolist()
+
+	# grad column names
+	col_names = df.columns.tolist()
+	return col_names, csv_values
